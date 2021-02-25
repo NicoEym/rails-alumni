379e149675c313781fdc29e02b4ede49f3ec73c6
nicolas <nicolas.eymenier@gmail.com>
2021-02-24 22:49:51 -0300

    set up the show page,  and improve the policies

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,12 +1,15 @@
 class UserPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.all
+      scope.where(profile: "Alumni")
     end
   end
 
-
   def update?
-    @user.admin?
+    user_profile_or_user_is_admin?
+  end
+
+  def user_profile_or_user_is_admin?
+    @user.admin? || @record == @user
   end
 end
